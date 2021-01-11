5aaffc6c4b383025219765a6143ee54ad4c34056
CrisKoch <cristinak.iftm@gmail.com>
2021-01-10 23:46:15 -0300

    atividade 09 - CRUD - novo

--- src/main/java/com/kochmann/demo_crud/controle/Controle.java
+++ src/main/java/com/kochmann/demo_crud/controle/Controle.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kochmann.demo_crud.dao.DisciplinaDao;
@@ -11,17 +13,30 @@ import com.kochmann.demo_crud.modelo.Disciplina;
 
 @Controller
 public class Controle {
-	
-	@RequestMapping("/home")
-	public String metodoMapeado() {
-		return "exemplo";
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
 	}
-	
+
 	@RequestMapping("/disciplinas")
 	public String exibirTabela(Model modelo) {
 		DisciplinaDao dao = new DisciplinaDao();
 		List<Disciplina> lista = dao.todos();
 		modelo.addAttribute("lista", lista);
 		return "disciplinas";
+	}
+
+	@GetMapping("/cadastra-disciplina")
+	public String exibeForm(Model modelo) {
+		modelo.addAttribute("disciplina", new Disciplina());
+		return "form-disciplina";
+	}
+
+	@PostMapping("/cadastra-disciplina")
+	public String processaForm(Disciplina disciplina) {
+		DisciplinaDao dao = new DisciplinaDao();
+		dao.inserir(disciplina);
+		return "redirect:/disciplinas";
 	}
 }
