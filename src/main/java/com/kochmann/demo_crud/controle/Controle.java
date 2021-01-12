1dfe7ad9d1e68df6fa11944ea18e9e76ec1048d9
CrisKoch <cristinak.iftm@gmail.com>
2021-01-12 00:19:46 -0300

    Crud funcionando - projeto novo

--- src/main/java/com/kochmann/demo_crud/controle/Controle.java
+++ src/main/java/com/kochmann/demo_crud/controle/Controle.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kochmann.demo_crud.dao.DisciplinaDao;
 import com.kochmann.demo_crud.modelo.Disciplina;
@@ -14,14 +15,20 @@ import com.kochmann.demo_crud.modelo.Disciplina;
 @Controller
 public class Controle {
 
-	@RequestMapping("/")
+	DisciplinaDao dao;
+	public Controle() {
+		super();
+		this.dao = new DisciplinaDao();
+	}
+	
+	
+	@RequestMapping("/home")
 	public String home() {
 		return "home";
 	}
 
-	@RequestMapping("/disciplinas")
+	@GetMapping("/disciplinas")
 	public String exibirTabela(Model modelo) {
-		DisciplinaDao dao = new DisciplinaDao();
 		List<Disciplina> lista = dao.todos();
 		modelo.addAttribute("lista", lista);
 		return "disciplinas";
@@ -35,8 +42,24 @@ public class Controle {
 
 	@PostMapping("/cadastra-disciplina")
 	public String processaForm(Disciplina disciplina) {
-		DisciplinaDao dao = new DisciplinaDao();
 		dao.inserir(disciplina);
+		return "redirect:/disciplinas";
+	}
+	@GetMapping("/excluir-disciplina")
+	public String excluirDisciplina(@RequestParam(name = "id", required = false) int id) {
+		dao.excluir(id);
+		return "redirect:/disciplinas";
+	}
+	
+	@GetMapping("/editar-disciplina")
+	public String editarDisciplina(Model modelo,@RequestParam(name = "id", required = false) int id) {
+		Disciplina disciplina = dao.buscaPorId(id);
+		modelo.addAttribute("disciplina",disciplina);
+		return "form-disciplina";
+	}
+	@PostMapping("/editar-disciplina")
+	public String editarDisciplina(Disciplina disciplina) {
+		dao.atualizar(disciplina);
 		return "redirect:/disciplinas";
 	}
 }
